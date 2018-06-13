d1b674a88c3ef9193f4bfeeaf60b153750022e5c
Atanas Atanasov <scriptex.bg@gmail.com>
2018-06-13 15:39:35 +0300

    Switch to postcss-preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -37,12 +37,15 @@ const svgoConfig = {
 };
 
 const postcssConfig = {
+	ident: 'postcss',
 	plugins: [
 		require('postcss-easy-import'),
 		require('postcss-url')({
 			url: 'rebase'
 		}),
-		require('postcss-cssnext'),
+		require('postcss-preset-env')({
+			stage: 0
+		}),
 		require('postcss-nested'),
 		require('postcss-mixins'),
 		require('postcss-utilities'),
