a2cb4fc9a8a05e369f5632292d71567b0ca69d62
Atanas Atanasov <scriptex.bg@gmail.com>
2020-07-27 15:51:44 +0300

    Upgrade packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 // @ts-nocheck
+
 const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
@@ -6,10 +7,10 @@ const { argv } = require('yargs');
 const { parse } = require('url');
 
 const { ProvidePlugin } = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const SpritesmithPlugin = require('webpack-spritesmith');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const WebpackShellPlugin = require('webpack-shell-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const { url, server, NODE_ENV } = argv;
@@ -124,8 +125,7 @@ const browserSyncConfig = {
 };
 
 const extractTextConfig = {
-	filename: 'dist/app.css',
-	allChunks: true
+	filename: 'dist/app.css'
 };
 
 const spritesmithConfig = {
@@ -160,14 +160,7 @@ module.exports = () => {
 	const isProduction = NODE_ENV === 'production';
 
 	if (isProduction) {
-		postcssConfig.plugins.push(
-			require('postcss-merge-rules'),
-			require('cssnano')({
-				discardComments: {
-					removeAll: true
-				}
-			})
-		);
+		postcssConfig.plugins.push(require('postcss-merge-rules'), require('cssnano')());
 	}
 
 	if (isDevelopment) {
@@ -192,19 +185,23 @@ module.exports = () => {
 		module: {
 			rules: [
 				{
-					test: /\.css$/,
-					use: ExtractTextPlugin.extract({
-						use: [
-							{
-								loader: 'css-loader',
-								options: sourceMap
-							},
-							{
-								loader: 'postcss-loader',
-								options: postcssConfig
+					test: /\.(sa|sc|c)ss$/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader,
+							options: {
+								hmr: isDevelopment
 							}
-						]
-					})
+						},
+						{
+							loader: 'css-loader',
+							options: sourceMap
+						},
+						{
+							loader: 'postcss-loader',
+							options: postcssConfig
+						}
+					]
 				},
 				{
 					test: /\.js$/,
@@ -233,7 +230,7 @@ module.exports = () => {
 				jQuery: 'jquery',
 				'window.jQuery': 'jquery'
 			}),
-			new ExtractTextPlugin(extractTextConfig),
+			new MiniCssExtractPlugin(extractTextConfig),
 			new SpritesmithPlugin(spritesmithConfig),
 			new CleanWebpackPlugin(cleanConfig),
 			new WebpackShellPlugin({
